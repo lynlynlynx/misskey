099628a079f020c2f68aacb33164c2332ab3f49e
Lynx Kotoura <admin@sanin.link>
2024-08-06 16:18:56 +0900

    Import components MkKeyValue and FormLink to about.emojis.vue

--- packages/frontend/src/pages/about.emojis.vue
+++ packages/frontend/src/pages/about.emojis.vue
@@ -59,6 +59,8 @@ import XEmoji from './emojis.emoji.vue';
 import MkButton from '@/components/MkButton.vue';
 import MkInput from '@/components/MkInput.vue';
 import MkFoldableSection from '@/components/MkFoldableSection.vue';
+import MkKeyValue from '@/components/MkKeyValue.vue';
+import FormLink from '@/components/form/link.vue';
 import { customEmojis, customEmojiCategories } from '@/custom-emojis.js';
 import { i18n } from '@/i18n.js';
 import { $i } from '@/i.js';
